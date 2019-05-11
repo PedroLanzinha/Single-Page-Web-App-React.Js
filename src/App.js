681e113ea377f5d9b253a179f91a93f83808a7c7
unknown <theitalianpeter@hotmail.com>
2019-05-11 23:53:08 +0100

    small commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <nav>
-        <h2 className="logo">Oscars 2019</h2>
+        <h2 className="logo"><a href="#">Oscars 2019</a></h2>
         <ul class="nav-menu">
           <li><a class="nav-menu__link" href="#">Home</a></li>
           <li><a class="nav-menu__link" href="#">Best Actor</a></li>
