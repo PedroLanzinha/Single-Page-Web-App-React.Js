1d76c0bfb439c3c2cd5abd9a3dde8e47d4f98466
unknown <theitalianpeter@hotmail.com>
2019-05-16 23:52:38 +0100

    small

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './ss/style.css';
 import Navbar from './components/navbar';
 import Home from './components/home';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar title="Oscars 2019" />
-      <Home title="Oscar Winners" />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar title="Oscars 2019" />
+        <Route to="/" component={Home} />
+        <Route to="/" render={() => <Home title="Oscar Winners" /> } />
+        {/*<Home title="Oscar Winners" /> */}
+      </div>
+    </BrowserRouter>
   );
 }
 
